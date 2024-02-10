7edc09b7f3a537d927e0972bcd30ac72ff402abc
s.cupelschii <seregakupel46@gmail.com>
2024-02-10 12:20:40 +0200

    chore: change examples for the success answer

--- src/HackerNews.Api/ResponseExamples/MessageBodyOkExample.cs
+++ src/HackerNews.Api/ResponseExamples/MessageBodyOkExample.cs
@@ -7,22 +7,24 @@ namespace HackerNews.Api.ResponseExamples;
 /// <summary>
 /// Message body for ok
 /// </summary>
-public class MessageBodyOkExample : IExamplesProvider<GetBestStories.BestStoriesResponse>
+public class MessageBodyOkExample : IExamplesProvider<IReadOnlyList<GetBestStories.BestStoriesResponse>>
 {
     /// <summary>
     /// Get examples
     /// </summary>
     /// <returns>Ok response</returns>
-    public GetBestStories.BestStoriesResponse GetExamples()
+    public IReadOnlyList<GetBestStories.BestStoriesResponse> GetExamples()
     {
-        return new GetBestStories.BestStoriesResponse
-        {
-            PostedBy = "dhouston",
-            CommentCount = 16,
-            Score = 111,
-            Time = DateTime.ParseExact("2021-10-10T10:10:10+00:00", "yyyy-MM-ddTHH:mm:ss+00:00", CultureInfo.InvariantCulture),
-            Title = "Some text",
-            Uri = new Uri("https://www.google.com")
-        };
+        return
+        [
+            new GetBestStories.BestStoriesResponse
+            {
+                PostedBy = "dhouston",
+                CommentCount = 16,
+                Score = 111,
+                Time = DateTime.Parse("2021-10-10T10:10:10+00:00", CultureInfo.InvariantCulture),
+                Title = "Some text"
+            }
+        ];
     }
 }
